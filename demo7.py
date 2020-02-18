ddb172868b48e74291a22be59789d9fbfbfe892b
hellokuls <42165104+hellokuls@users.noreply.github.com>
2020-02-18 19:07:54 +0800

    激活码

---
+++ demo7.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+url = "http://lookdiv.com/nssdh/sereas/sxclo/aloif/smxs/slak/pdoasj/ejekoq/ewqqzsd/wsdwwq/ers.html"
+header = {
+    'Connection': 'close',
+    'User-Agent': 'Mozilla / 5.0(WindowsNT10.0;Win64;x64;rv: 65.0) Gecko / 20100101Firefox / 65.0'
+}
+data = {
+    "key": "lookdiv.com"
+}
+html = requests.post(url=url, data=data, headers=header).text
+soup = BeautifulSoup(html, 'html.parser')
+zhucema = soup.textarea.text
+with open('注册码.txt', 'a') as file_handle:
+    file_handle.write(zhucema)
+    file_handle.write('\n')
+print(zhucema)
